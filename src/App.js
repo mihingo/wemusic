4d0e369bf436bfc081ad8c0a50681b713f8f382b
Mihingo Kelvin N <kmihingo@microsoft.com>
2021-04-01 17:24:13 +0300

    Adding Spotify sidebar and footer - play, pause, forward etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./wemusic";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  //const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      // setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+        console.log("myself&I", user);
+      });
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+    }
+  }, [dispatch]);
+
+  console.log("User>> ", user);
   return (
     <div className="App">
-      <Login />
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,35 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  // token:
+  //   "BQDwGu_zDYv7Kf3taGBCrD5a9mUdk7t5_t8r0aLfECs2YT9ESnYwYrGUBkTB7WMPaDRrXbDiC4meyT7aG2saOT6MkHgkDnnJnSYjgAZTPuenGc22zwHZB49s5r5jHbf33WVS8HZgUFmYrg1oMisIXHXV78G4VcqclBYT1WmqSS8DQJwO",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
