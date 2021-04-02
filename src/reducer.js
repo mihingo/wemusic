87ab2e87130729dcf45c9fae36fd39f2273d6c14
Mihingo Kelvin N <kmihingo@microsoft.com>
2021-04-02 10:59:08 +0300

    Adding Spotify sidebar and footer - play, pause, forward etc

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  // token:
-  //   "BQDwGu_zDYv7Kf3taGBCrD5a9mUdk7t5_t8r0aLfECs2YT9ESnYwYrGUBkTB7WMPaDRrXbDiC4meyT7aG2saOT6MkHgkDnnJnSYjgAZTPuenGc22zwHZB49s5r5jHbf33WVS8HZgUFmYrg1oMisIXHXV78G4VcqclBYT1WmqSS8DQJwO",
+  token:
+    "BQDwGu_zDYv7Kf3taGBCrD5a9mUdk7t5_t8r0aLfECs2YT9ESnYwYrGUBkTB7WMPaDRrXbDiC4meyT7aG2saOT6MkHgkDnnJnSYjgAZTPuenGc22zwHZB49s5r5jHbf33WVS8HZgUFmYrg1oMisIXHXV78G4VcqclBYT1WmqSS8DQJwO",
 };
 
 const reducer = (state, action) => {
